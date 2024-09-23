083437fee5f6514bfd5b10446331cd5ed5cfa28e
Gc <giovacalle.dev@gmail.com>
2024-09-24 01:31:08 +0200

    feat: start lucia config

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,80 +1,23 @@
-import type { AdapterAccountType } from 'next-auth/adapters';
+import { pgEnum, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 
-import { boolean, integer, pgTable, primaryKey, text, timestamp } from 'drizzle-orm/pg-core';
+const providers = pgEnum('auth_providers', ['email', 'google']);
 
 export const usersTable = pgTable('users', {
-  id: text('id')
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  name: text('name'),
-  email: text('email').unique(),
-  emailVerified: timestamp('emailVerified', { mode: 'date' }),
-  image: text('image')
+  id: uuid('id').defaultRandom().primaryKey(),
+  provider: providers('provider').notNull(),
+  providerId: text('provideId').unique(),
+  email: text('email').notNull().unique(),
+  password_hash: text('password_hash'),
+  created_at: timestamp('created_at').defaultNow()
 });
-
-export const accountsTable = pgTable(
-  'accounts',
-  {
-    userId: text('userId')
-      .notNull()
-      .references(() => usersTable.id, { onDelete: 'cascade' }),
-    type: text('type').$type<AdapterAccountType>().notNull(),
-    provider: text('provider').notNull(),
-    providerAccountId: text('providerAccountId').notNull(),
-    refresh_token: text('refresh_token'),
-    access_token: text('access_token'),
-    expires_at: integer('expires_at'),
-    token_type: text('token_type'),
-    scope: text('scope'),
-    id_token: text('id_token'),
-    session_state: text('session_state')
-  },
-  accounts => ({
-    compoundKey: primaryKey({
-      columns: [accounts.provider, accounts.providerAccountId]
-    })
-  })
-);
 
 export const sessionsTable = pgTable('sessions', {
-  sessionToken: text('sessionToken').primaryKey(),
-  userId: text('userId')
+  id: text('id').primaryKey(),
+  userId: uuid('user_id')
     .notNull()
-    .references(() => usersTable.id, { onDelete: 'cascade' }),
-  expires: timestamp('expires', { mode: 'date' }).notNull()
+    .references(() => usersTable.id, {
+      onDelete: 'cascade'
+    }), // camelCased because it is required by the Lucia package
+  expiresAt: timestamp('expires_at').notNull(), // camelCased because it is required by the Lucia package
+  created_at: timestamp('created_at').defaultNow()
 });
-
-export const verificationTokensTable = pgTable(
-  'verificationTokens',
-  {
-    identifier: text('identifier').notNull(),
-    token: text('token').notNull(),
-    expires: timestamp('expires', { mode: 'date' }).notNull()
-  },
-  verificationTokens => ({
-    compositePk: primaryKey({
-      columns: [verificationTokens.identifier, verificationTokens.token]
-    })
-  })
-);
-
-export const authenticatorsTable = pgTable(
-  'authenticators',
-  {
-    credentialID: text('credentialID').notNull().unique(),
-    userId: text('userId')
-      .notNull()
-      .references(() => usersTable.id, { onDelete: 'cascade' }),
-    providerAccountId: text('providerAccountId').notNull(),
-    credentialPublicKey: text('credentialPublicKey').notNull(),
-    counter: integer('counter').notNull(),
-    credentialDeviceType: text('credentialDeviceType').notNull(),
-    credentialBackedUp: boolean('credentialBackedUp').notNull(),
-    transports: text('transports')
-  },
-  authenticators => ({
-    compositePK: primaryKey({
-      columns: [authenticators.userId, authenticators.credentialID]
-    })
-  })
-);
---
+++ src/lib/auth.ts
@@ -0,0 +1,54 @@
+import { cookies } from 'next/headers';
+
+import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
+import { Lucia } from 'lucia';
+import { User } from 'lucia';
+import { Session } from 'lucia';
+
+import { db } from '@/db/db';
+import { sessionsTable, usersTable } from '@/db/schema';
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessionsTable, usersTable);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict'
+    }
+  }
+});
+
+export const validateRequest = async (): Promise<
+  { user: User; session: Session } | { user: null; session: null }
+> => {
+  const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null
+    };
+  }
+
+  const result = await lucia.validateSession(sessionId);
+
+  // next.js throws when you attempt to set cookie when rendering page
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+    }
+  } catch {}
+  return result;
+};
+
+declare module 'lucia' {
+  interface Register {
+    Lucia: typeof lucia;
+  }
+}
