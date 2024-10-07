e3c8a4448e09806123cc3d32bfdcf386da7cdbec
Gc <giovacalle.dev@gmail.com>
2024-10-07 19:36:52 +0200

    feat: add basic sign-out page (+ upd next)

--- src/components/layout/actions.ts
+++ src/components/layout/actions.ts
@@ -8,12 +8,12 @@ import { lucia, validateRequest } from '@/lib/auth';
 export async function signOut() {
   const { session } = await validateRequest();
 
-  if (!session) throw new Error('Not authenticated');
+  if (!session) redirect('/sign-in');
 
   await lucia.invalidateSession(session.id);
 
   const sessionCookie = lucia.createBlankSessionCookie();
   cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
 
-  redirect('/sign-in');
+  redirect('/sign-out');
 }
